8c3d2384fdbe9518d4c64ccec82bec4fcaeb9b15
moyel <1010404561@qq.com>
2023-07-09 10:30:07 +0800

    page: 测试page

---
+++ src/main/java/top/moyel/mayo/constant/PageConstant.java
@@ -0,0 +1,9 @@
+package top.moyel.mayo.constant;
+
+/**
+ * @author moyel
+ */
+public class PageConstant {
+    public static final String DEFAULT_PAGE_STR = "1";
+    public static final String DEFAULT_PAGE_SIZE_STR = "10";
+}
--- src/main/java/top/moyel/mayo/modules/sys/rest/SysUserRest.java
+++ src/main/java/top/moyel/mayo/modules/sys/rest/SysUserRest.java
@@ -1,9 +1,13 @@
 package top.moyel.mayo.modules.sys.rest;
 
+import com.mybatisflex.core.paginate.Page;
+import com.mybatisflex.core.query.QueryWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import top.moyel.mayo.constant.PageConstant;
 import top.moyel.mayo.modules.sys.entity.SysUser;
 import top.moyel.mayo.modules.sys.mapstruct.SysUserMapStruct;
 import top.moyel.mayo.modules.sys.service.ISysUserService;
@@ -27,5 +31,14 @@ public class SysUserRest {
     public SysUserVO fetch() {
         Optional<SysUser> sysUserOptional = sysUserService.getOneOpt(SYS_USER.ID.eq(1L));
         return SysUserMapStruct.INSTANCE.toVO(sysUserOptional.orElse(null));
+    }
+
+    @GetMapping("/page")
+    public Page<SysUserVO> page(
+            @RequestParam(defaultValue = PageConstant.DEFAULT_PAGE_STR) Integer page,
+            @RequestParam(defaultValue = PageConstant.DEFAULT_PAGE_SIZE_STR) Integer pageSize
+    ) {
+        Page<SysUserVO> pageImpl = Page.of(page, pageSize);
+        return sysUserService.pageAs(pageImpl, QueryWrapper.create(), SysUserVO.class);
     }
 }
