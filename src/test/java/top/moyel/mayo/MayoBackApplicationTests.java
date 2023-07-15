635203b38a61594168f4cdf0b51b5e6737c9e9ef
moyel <1010404561@qq.com>
2023-07-15 15:37:13 +0800

    test: 测试获取泛型参数

--- src/test/java/top/moyel/mayo/MayoBackApplicationTests.java
+++ src/test/java/top/moyel/mayo/MayoBackApplicationTests.java
@@ -1,13 +1,20 @@
 package top.moyel.mayo;
 
+import cn.hutool.core.util.TypeUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.moyel.mayo.modules.sys.service.impl.SysUserServiceImpl;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
 
 @SpringBootTest
 class MayoBackApplicationTests {
 
     @Test
     void contextLoads() {
+        Type[] typeArguments = TypeUtil.getTypeArguments(SysUserServiceImpl.class);
+        System.out.println(Arrays.toString(typeArguments));
     }
 
 }
