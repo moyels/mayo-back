18ea77c1c9960907450f2a4c380f89d8098ff637
moyel <1010404561@qq.com>
2023-07-18 21:40:43 +0800

    test: excel响应的测试

--- src/main/java/top/moyel/mayo/modules/sys/rest/ExcelRest.java
+++ src/main/java/top/moyel/mayo/modules/sys/rest/ExcelRest.java
@@ -1,9 +1,11 @@
 package top.moyel.mayo.modules.sys.rest;
 
 import com.pig4cloud.plugin.excel.annotation.RequestExcel;
+import com.pig4cloud.plugin.excel.annotation.ResponseExcel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.moyel.mayo.annotation.NoResult;
 import top.moyel.mayo.modules.sys.vo.ExcelTestVO;
 
 import java.util.List;
@@ -21,8 +23,10 @@ public class ExcelRest {
      *
      * @param excel excel文件解析结果
      */
+    @NoResult
+    @ResponseExcel
     @PostMapping("/test")
-    public void test(@RequestExcel List<ExcelTestVO> excel) {
-        System.out.println(excel);
+    public List<ExcelTestVO> test(@RequestExcel List<ExcelTestVO> excel) {
+        return excel;
     }
 }
